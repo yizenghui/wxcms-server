fbbda6396279f3b8adbc1126b77af020b33fcdb3
yizenghui <245561237@qq.com>
2019-03-19 17:46:20 +0800

    save..

--- app/Http/Controllers/Api/ActionController.php
+++ app/Http/Controllers/Api/ActionController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Fan;
 
 class ActionController extends Controller
 {
 
     public function view(Request $request){
-        
-        // $topic = Topic::findOrFail($id);
-        
-        return response()->json($request->user());
+        $article = Article::findOrFail($request->get('article_id'));
+        // $this->userView($request->user(),$article);
+        return response()->json($this->userView($request->user(),$article));
     }
 
+    public function userView(Fan $user,Article $article){
+
+        $article->view ++;
+        $article->save();
+        return '';
+    }
 }
