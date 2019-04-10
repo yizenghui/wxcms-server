7d1ea9bb4c1012aa64567bf9acfae5a0ce50e7c8
yizenghui <245561237@qq.com>
2019-04-10 17:54:37 +0800

    首页作为介绍

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>WxCMS</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    WxCMS
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">投稿</a>
+                    <a href="#">推广合作</a>
+                    <a href="#">价格</a>
+                    <a href="/admin">管理员</a>
+                    <a href="https://github.com/yizenghui/wxcms">GitHub</a>
                 </div>
             </div>
         </div>
