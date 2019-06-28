af7ce11bfd65646face0970568176905b965bbf5
yizenghui <245561237@qq.com>
2019-06-28 15:06:48 +0800

    去掉 api/user 测试接口

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // Route::group(['middleware' => ['force-json', 'auth:api']], function () {
 //     // put your router
