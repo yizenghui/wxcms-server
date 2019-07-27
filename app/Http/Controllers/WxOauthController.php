40da343681d32f2ea6ef974cdaea1410a019ac1a
yizenghui <245561237@qq.com>
2019-07-27 15:44:25 +0800

    换用appid好交流

--- app/Http/Controllers/WxOauthController.php
+++ app/Http/Controllers/WxOauthController.php
@@ -35,7 +35,7 @@ class WxOauthController extends Controller
             $app->refresh_token = $refresh_token;
             //记录refresh_token到数据库
             $app->save();
-            return redirect('wxoauth/commitCode?app_id='.$app_id.'&refresh_token='.$refresh_token);
+            return redirect('wxoauth/commitCode?appid='.$app->id);
         }else{ // 不正常的
             return "出错了，无法匹配到相应的AppID";
         }
@@ -50,36 +50,38 @@ class WxOauthController extends Controller
 
 
     public function commitCode(Request $request){ // 提交代码
-        $app_id = $request->get('app_id');
-        $refresh_token = $request->get('refresh_token');
-        // $app = App::find(1);
-        // $app_id = $app->app_id;//$request->get('app_id');
-        // $refresh_token = $app->refresh_token; //$request->get('refresh_token');
-
+        $appid = $request->get('appid');
+        $app = App::find(intval($appid));
+        $app_id = $app->app_id;//$request->get('app_id');
+        $refresh_token = $app->refresh_token; //$request->get('refresh_token');
+        if(!$app_id || $refresh_token) return redirect('wxoauth'); //没有app_id或refresh_token,都去跑授权
+        // $app_id = "wxa94ddd94358b2d1d";$refresh_token = "refreshtoken@@@NlYaCEWLcSwu5VpBCEf6b9b_Y57sbo7lL2Qm1x1T79M";
         $openPlatform = \EasyWeChat::openPlatform(); // 开放平台
         $c = $openPlatform->code_template; // 模板
         $l = $c->list(); // 模板列表
         $template_id = $l["template_list"][0]["template_id"]; //模板列表顺位第一模板id
         $miniProgram = $openPlatform->miniProgram($app_id, $refresh_token);
+
         $version = config('point.mini_program_version');
         $version_desc = config('point.mini_program_version_desc');
         $ret = $miniProgram->code->commit($template_id,json_encode(['api_token'=>Hashids::encode($app->id,date('Ymd')), 'app_name'=>$app->app_name]), $version, $version_desc);
-        if(0){ // todo 提交代码失败
-
+        if(1){ // todo 提交代码失败
+            $app->current_version = $version; //当前提交版本
+            $app->save();
         }
         // return $ret;
-        $qrcode = 'wxoauth/getQrCode?app_id='.$app_id.'&refresh_token='.$refresh_token;
+        $qrcode = 'wxoauth/getQrCode?appid='.$appid;
         // 跳转去获取体验版二维码
         return view('welcome',compact('qrcode'));
     }
 
 
     public function getQrCode(Request $request){ // 提交代码
-        $app_id = $request->get('app_id');
-        $refresh_token = $request->get('refresh_token');
-        $app = App::find(1);
+        $appid = $request->get('appid');
+        $app = App::find(intval($appid));
         $app_id = $app->app_id;//$request->get('app_id');
         $refresh_token = $app->refresh_token; //$request->get('refresh_token');
+        if(!$app_id || $refresh_token) return 'falt'; //没有app_id或refresh_token
         $openPlatform = \EasyWeChat::openPlatform(); // 开放平台
         $miniProgram = $openPlatform->miniProgram($app_id, $refresh_token);
         return $miniProgram->code->getQrCode("/pages/index/index");
