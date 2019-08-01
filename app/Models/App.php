07c661293862a43df96fcf4d9006a7f6a179906a
yizenghui <zenghuitrue@gmail.com>
2019-08-01 23:44:16 +0800

    ss

--- app/Models/App.php
+++ app/Models/App.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
+use Vinkla\Hashids\Facades\Hashids;
 
 class App extends Model
 {
